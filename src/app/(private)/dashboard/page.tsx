76f45ca2d344ce719b6480c5354012a3efe51307
Wesley Rafael <wesley.santos@opah.com>
2025-07-22 22:43:09 -0300

    feat: init dashboard layout

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,11 +1,34 @@
-import { logOutAction } from '@/features/auth/action';
+import { Logo } from '@/components/atoms';
 
 const Dashboard = () => {
 	return (
-		<>
-			<p>dashboard</p>
-			<button onClick={logOutAction}>sair</button>
-		</>
+		<div className="flex justify-center">
+			<div className="w-4xl flex flex-col gap-7">
+				<div className="bg-white p-4 rounded-b-3xl">
+					<Logo type="secondary" />
+				</div>
+				<div className="bg-white rounded-3xl p-4">
+					<div>
+						<p>grupos</p>
+						<div className="relative w-60 mt-3.5">
+							<div className="absolute -top-3 left-4 z-10">
+								<div className="bg-[#8970ff] rounded-t-lg px-6 py-1 shadow-sm">
+									<div className="w-12 h-2"></div>
+								</div>
+							</div>
+
+							<div className="relative bg-[#7462f0] rounded-lg shadow-lg min-h-32 p-6">
+								<div className="absolute inset-0 bg-gradient-to-br from-[#5f54db]/20 to-[#4c48bb]/10 rounded-lg pointer-events-none"></div>
+
+								<div className="relative z-10">oi</div>
+
+								<div className="absolute -bottom-1 -right-1 w-full h-full bg-[#4c48bb]/30 rounded-lg -z-10"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
--- src/components/atoms/logo/logo.tsx
+++ src/components/atoms/logo/logo.tsx
@@ -1,11 +1,29 @@
 import Link from 'next/link';
 
-const Logo = () => {
-	return (
-		<Link href="/">
-			<h1 className="text-4xl font-bold bg-gradient-to-r from-white to-blue-200 bg-clip-text text-transparent">
+interface ILogoProps {
+	type?: 'primary' | 'secondary';
+}
+
+const Logo = ({ type = 'primary' }: ILogoProps) => {
+	const getLogoUi = () => {
+		if (type === 'secondary')
+			return (
+				<h1 className="text-3xl font-bold text-gray-900">
+					<span className="bg-gradient-to-r from-blue-900 to-purple-700 bg-clip-text text-transparent">
+						Dropa
+					</span>
+				</h1>
+			);
+
+		return (
+			<h1 className="text-3xl font-bold bg-gradient-to-r from-white to-blue-200 bg-clip-text text-transparent">
 				Dropa
 			</h1>
+		);
+	};
+	return (
+		<Link href="/" className="inline-block">
+			{getLogoUi()}
 		</Link>
 	);
 };
