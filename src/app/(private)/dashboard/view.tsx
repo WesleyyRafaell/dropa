f1aa996e6566819602f9f13eedd5574854578ac3
Wesley Rafael <wesley.santos@opah.com>
2025-11-17 12:00:33 -0300

    feat: add email on dashboard header and add vercel speedInsights

--- src/app/(private)/dashboard/view.tsx
+++ src/app/(private)/dashboard/view.tsx
@@ -5,12 +5,15 @@ import { VscNewFolder } from 'react-icons/vsc';
 import { IGroup } from '@/features/groups/models';
 import useDashboard from './useDashboard';
 import { FileManagerPanel } from '@/components/organisms';
+import { IoIosLogOut } from 'react-icons/io';
+import { useUserStore } from '@/store/user-store';
 
 export interface IViewProps {
 	groups?: IGroup[];
 }
 
 const View = ({ groups }: IViewProps) => {
+	const { user } = useUserStore();
 	const {
 		groupList,
 		handleCreteNewGroup,
@@ -25,12 +28,23 @@ const View = ({ groups }: IViewProps) => {
 	return (
 		<div className="flex justify-center">
 			<div className="w-4xl flex flex-col gap-7">
-				<div className="flex items-center justify-between bg-white p-4 rounded-b-3xl">
+				<div className=" bg-white p-4 rounded-b-3xl">
 					<Logo type="secondary" />
 
-					<button onClick={logOut} className="btn btn-primary btn-outline border-0">
-						Sair
-					</button>
+					<div className="flex items-center justify-between">
+						<div>
+							<p className="text-xs text-gray-600">{user?.email}</p>
+						</div>
+
+						<div className="tooltip max-w-40" data-tip="Sair">
+							<div
+								onClick={logOut}
+								className="w-11 h-11 bg-white cursor-pointer flex justify-center items-center rounded-full hover:bg-primary text-primary hover:text-white transition-all"
+							>
+								<IoIosLogOut className="text-2xl" />
+							</div>
+						</div>
+					</div>
 				</div>
 				<div className="flex flex-col gap-7">
 					<div className="bg-white rounded-3xl p-4">
