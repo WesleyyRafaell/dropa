1147044c7537ad16fe64ee66b06bc1c08cce49cb
Wesley Rafael <wesley.santos@opah.com>
2025-11-20 12:14:34 -0300

    feat: responsive

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -13,16 +13,26 @@ const Header = ({ email, logOut }: IHeaderProps) => {
 	const { currentLimiteUploadSize } = CurrentLimiteUploadSizeStore();
 
 	return (
-		<div className=" bg-white p-4 rounded-b-3xl">
-			<Logo type="secondary" />
+		<div className="bg-white p-4 rounded-b-3xl">
+			<div className="flex items-center justify-between mb-3">
+				<Logo type="secondary" />
+				<div className="tooltip tooltip-left max-w-40" data-tip="Sair">
+					<div
+						onClick={logOut}
+						className="w-11 h-11 bg-white cursor-pointer flex justify-center items-center rounded-full hover:bg-primary text-primary hover:text-white transition-all"
+					>
+						<IoIosLogOut className="text-2xl" />
+					</div>
+				</div>
+			</div>
 
-			<div className="flex items-center justify-between">
+			<div className="flex flex-col md:flex-row items-center gap-2 justify-between">
 				<div>
 					<p className="text-xs text-gray-600">{email || ''}</p>
 				</div>
 
-				<div className="flex items-center gap-5">
-					<div>
+				<div className="flex flex-col md:flex-row items-center gap-5">
+					<div className="flex flex-col items-center md:items-start gap-1.5 md:gap-1">
 						<progress
 							className="progress progress-primary w-56"
 							value={currentLimiteUploadSize}
@@ -32,14 +42,6 @@ const Header = ({ email, logOut }: IHeaderProps) => {
 							{formatFileSize(currentLimiteUploadSize)} de{' '}
 							{formatFileSize(MAX_UPLOAD_LIMIT_FREE_ACCOUNT)}
 						</p>
-					</div>
-					<div className="tooltip max-w-40" data-tip="Sair">
-						<div
-							onClick={logOut}
-							className="w-11 h-11 bg-white cursor-pointer flex justify-center items-center rounded-full hover:bg-primary text-primary hover:text-white transition-all"
-						>
-							<IoIosLogOut className="text-2xl" />
-						</div>
 					</div>
 				</div>
 			</div>
